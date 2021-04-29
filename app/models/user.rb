4dc15b5bdcd84d0044dd1f012605ffef3718f85b
Paul Ladd <vlendle@gmail.com>
2021-04-29 22:25:34 +0000

    Migrate DB and change "user" to "users"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
-  validates email, confirmation: true
+
+  validates :email, confirmation: true
   validates :email_confirmation, presence: true
+
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
