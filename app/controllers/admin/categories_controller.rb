0c4d1803676f50c41039ef2b513cdeff66f7e25a
Paul Ladd <vlendle@gmail.com>
2021-04-29 18:32:49 +0000

    Add #create and #new actions for creating categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -8,5 +8,18 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'New category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
