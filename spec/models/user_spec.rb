4635bcd05b949ff3ed353be5e099aeaf99681bab
Paul Ladd <vlendle@gmail.com>
2021-05-03 21:20:01 +0000

    Add email uniqueness and confirmation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,15 +4,16 @@ RSpec.describe User, type: :model do
   describe 'validations:' do
     describe 'password' do
       it 'should require both password and password_confirmation' do
-        user1 = User.create password: 'banana',
+        user = User.create password: 'banana',
                             email: 'test@test.com',
                             email_confirmation: 'test@test.com'
-        expect(user1.errors.full_messages).to include "Password confirmation can't be blank"
-        user2 = User.create password_confirmation: 'banana',
+        expect(user.errors.full_messages).to include "Password confirmation can't be blank"
+        user = User.create password_confirmation: 'banana',
                             email: 'test@test.com',
                             email_confirmation: 'test@test.com'
-        expect(user2.errors.full_messages).to include "Password can't be blank"
+        expect(user.errors.full_messages).to include "Password can't be blank"
       end
+
       it 'should require that password and password_confirmation match' do
         user = User.create  password: 'banana',
                             password_confirmation: 'ananab',
@@ -20,6 +21,7 @@ RSpec.describe User, type: :model do
                             email_confirmation: 'test@test.com'
         expect(user.errors.full_messages).to include "Password confirmation doesn't match Password"
       end
+
       it 'succeeds when password and password_confirmation exist and match' do
         user = User.create  password: 'banana',
                             password_confirmation: 'banana',
@@ -42,6 +44,32 @@ RSpec.describe User, type: :model do
         expect(user2.errors.full_messages).to include "Email has already been taken"
       end
 
+      it 'should require both email and email_confirmation' do
+        user = User.create  password: 'banana',
+                            password_confirmation: 'banana',
+                            email: 'test@test.com'
+        expect(user.errors.full_messages).to include "Email confirmation can't be blank"
+        user = User.create  password: 'banana',
+                            password_confirmation: 'banana',
+                            email_confirmation: 'test@test.com'
+        expect(user.errors.full_messages).to include "Email confirmation doesn't match Email"
+      end
+
+      it 'should require that email and email_confirmation match' do
+        user = User.create  password: 'banana',
+                            password_confirmation: 'banana',
+                            email: 'test@test.com',
+                            email_confirmation: 'example@example.com'
+        expect(user.errors.full_messages).to include "Email confirmation doesn't match Email"
+      end
+
+      it 'succeeds when email and email_confirmation exist and match' do
+        user = User.create  password: 'banana',
+                            password_confirmation: 'banana',
+                            email: 'test@test.com',
+                            email_confirmation: 'test@test.com'
+        expect(user).to be_valid
+      end
     end
   end
 end
