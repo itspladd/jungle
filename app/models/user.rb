9e57b2740ebb2b1c21a54c626f6a9d24aedd6cf6
Paul Ladd <vlendle@gmail.com>
2021-05-03 23:45:27 +0000

    Add first_name validation and testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
+
+  validates :first_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,15 @@ RSpec.describe User, type: :model do
   describe 'validations:' do
     describe 'password' do
       it 'should require both password and password_confirmation' do
-        user = User.create password: 'banana',
+        user = User.create  password: 'banana',
                             email: 'test@test.com',
-                            email_confirmation: 'test@test.com'
+                            email_confirmation: 'test@test.com',
+                            first_name: 'Pringles'
         expect(user.errors.full_messages).to include "Password confirmation can't be blank"
-        user = User.create password_confirmation: 'banana',
+        user = User.create  password_confirmation: 'banana',
                             email: 'test@test.com',
-                            email_confirmation: 'test@test.com'
+                            email_confirmation: 'test@test.com',
+                            first_name: 'Pringles'
         expect(user.errors.full_messages).to include "Password can't be blank"
       end
 
@@ -18,7 +20,8 @@ RSpec.describe User, type: :model do
         user = User.create  password: 'banana',
                             password_confirmation: 'ananab',
                             email: 'test@test.com',
-                            email_confirmation: 'test@test.com'
+                            email_confirmation: 'test@test.com',
+                            first_name: 'Pringles'
         expect(user.errors.full_messages).to include "Password confirmation doesn't match Password"
       end
 
@@ -26,7 +29,8 @@ RSpec.describe User, type: :model do
         user = User.create  password: 'banana',
                             password_confirmation: 'banana',
                             email: 'test@test.com',
-                            email_confirmation: 'test@test.com'
+                            email_confirmation: 'test@test.com',
+                            first_name: 'Pringles'
         expect(user).to be_valid
       end
     end
@@ -36,22 +40,26 @@ RSpec.describe User, type: :model do
         user1 = User.create password: 'banana',
                             password_confirmation: 'banana',
                             email: 'test@test.com',
-                            email_confirmation: 'test@test.com'
+                            email_confirmation: 'test@test.com',
+                            first_name: 'Doritos'
         user2 = User.create password: 'banana',
                             password_confirmation: 'banana',
                             email: 'test@test.com',
-                            email_confirmation: 'test@test.com'
+                            email_confirmation: 'test@test.com',
+                            first_name: 'Doritos'
         expect(user2.errors.full_messages).to include "Email has already been taken"
       end
 
       it 'should require both email and email_confirmation' do
         user = User.create  password: 'banana',
                             password_confirmation: 'banana',
-                            email: 'test@test.com'
+                            email: 'test@test.com',
+                            first_name: 'Doritos'
         expect(user.errors.full_messages).to include "Email confirmation can't be blank"
         user = User.create  password: 'banana',
                             password_confirmation: 'banana',
-                            email_confirmation: 'test@test.com'
+                            email_confirmation: 'test@test.com',
+                            first_name: 'Doritos'
         expect(user.errors.full_messages).to include "Email confirmation doesn't match Email"
       end
 
@@ -59,7 +67,8 @@ RSpec.describe User, type: :model do
         user = User.create  password: 'banana',
                             password_confirmation: 'banana',
                             email: 'test@test.com',
-                            email_confirmation: 'example@example.com'
+                            email_confirmation: 'example@example.com',
+                            first_name: 'Doritos'
         expect(user.errors.full_messages).to include "Email confirmation doesn't match Email"
       end
 
@@ -67,8 +76,19 @@ RSpec.describe User, type: :model do
         user = User.create  password: 'banana',
                             password_confirmation: 'banana',
                             email: 'test@test.com',
-                            email_confirmation: 'test@test.com'
+                            email_confirmation: 'test@test.com',
+                            first_name: 'Doritos'
         expect(user).to be_valid
+      end
+    end
+
+    describe 'names' do
+      it 'should include first_name' do
+        user = User.create  password: 'banana',
+                            password_confirmation: 'banana',
+                            email: 'test@test.com',
+                            email_confirmation: 'test@test.com'
+        expect(user.errors.full_messages).to include "First name can't be blank"
       end
     end
   end
