6f6f282eb29311f94696c997154b3a3f7d188a9a
Paul Ladd <vlendle@gmail.com>
2021-05-04 00:01:42 +0000

    Refactor user_spec to use an object of consistent parameters. Add first_name and last_name tests and validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,93 +2,65 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations:' do
+    before(:each) do
+      @params = {
+        first_name: 'Tautrion',
+        last_name: 'Stoneglutes',
+        email: 'goddess-king@petalsworn.clan',
+        email_confirmation: 'goddess-king@petalsworn.clan',
+        password: 'flowerDeity96',
+        password_confirmation: 'flowerDeity96'
+      }
+    end
+
+    it 'should succeed when all parameters/confirmations are valid/present' do
+      user = User.create @params
+      expect(user).to be_valid
+    end
+
     describe 'password' do
       it 'should require both password and password_confirmation' do
-        user = User.create  password: 'banana',
-                            email: 'test@test.com',
-                            email_confirmation: 'test@test.com',
-                            first_name: 'Pringles'
-        expect(user.errors.full_messages).to include "Password confirmation can't be blank"
-        user = User.create  password_confirmation: 'banana',
-                            email: 'test@test.com',
-                            email_confirmation: 'test@test.com',
-                            first_name: 'Pringles'
-        expect(user.errors.full_messages).to include "Password can't be blank"
+        user = User.create @params.except(:password_confirmation)
+        expect(user.errors.full_messages)
+          .to include "Password confirmation can't be blank"
+        user = User.create @params.except(:password)
+        expect(user.errors.full_messages)
+          .to include "Password can't be blank"
       end
-
       it 'should require that password and password_confirmation match' do
-        user = User.create  password: 'banana',
-                            password_confirmation: 'ananab',
-                            email: 'test@test.com',
-                            email_confirmation: 'test@test.com',
-                            first_name: 'Pringles'
+        @params[:password] = 'banana'
+        user = User.create @params
         expect(user.errors.full_messages).to include "Password confirmation doesn't match Password"
-      end
-
-      it 'succeeds when password and password_confirmation exist and match' do
-        user = User.create  password: 'banana',
-                            password_confirmation: 'banana',
-                            email: 'test@test.com',
-                            email_confirmation: 'test@test.com',
-                            first_name: 'Pringles'
-        expect(user).to be_valid
       end
     end
 
     describe 'email' do
       it 'should require a unique email' do
-        user1 = User.create password: 'banana',
-                            password_confirmation: 'banana',
-                            email: 'test@test.com',
-                            email_confirmation: 'test@test.com',
-                            first_name: 'Doritos'
-        user2 = User.create password: 'banana',
-                            password_confirmation: 'banana',
-                            email: 'test@test.com',
-                            email_confirmation: 'test@test.com',
-                            first_name: 'Doritos'
+        user1 = User.create @params
+        user2 = User.create @params
         expect(user2.errors.full_messages).to include "Email has already been taken"
       end
-
       it 'should require both email and email_confirmation' do
-        user = User.create  password: 'banana',
-                            password_confirmation: 'banana',
-                            email: 'test@test.com',
-                            first_name: 'Doritos'
+        user = User.create @params.except(:email_confirmation)
         expect(user.errors.full_messages).to include "Email confirmation can't be blank"
-        user = User.create  password: 'banana',
-                            password_confirmation: 'banana',
-                            email_confirmation: 'test@test.com',
-                            first_name: 'Doritos'
+        user = User.create @params.except(:email)
         expect(user.errors.full_messages).to include "Email confirmation doesn't match Email"
       end
-
       it 'should require that email and email_confirmation match' do
-        user = User.create  password: 'banana',
-                            password_confirmation: 'banana',
-                            email: 'test@test.com',
-                            email_confirmation: 'example@example.com',
-                            first_name: 'Doritos'
+        @params[:email] = 'bloodwarrior@hemoclobberin.clan'
+        user = User.create @params
         expect(user.errors.full_messages).to include "Email confirmation doesn't match Email"
-      end
-
-      it 'succeeds when email and email_confirmation exist and match' do
-        user = User.create  password: 'banana',
-                            password_confirmation: 'banana',
-                            email: 'test@test.com',
-                            email_confirmation: 'test@test.com',
-                            first_name: 'Doritos'
-        expect(user).to be_valid
       end
     end
 
     describe 'names' do
-      it 'should include first_name' do
-        user = User.create  password: 'banana',
-                            password_confirmation: 'banana',
-                            email: 'test@test.com',
-                            email_confirmation: 'test@test.com'
+      it 'should require first_name' do
+        user = User.create @params.except(:first_name)
         expect(user.errors.full_messages).to include "First name can't be blank"
+      end
+      it 'should require last_name' do
+        user = User.create @params.except(:last_name)
+        expect(user.errors.full_messages).to include "Last name can't be blank"
       end
     end
   end
